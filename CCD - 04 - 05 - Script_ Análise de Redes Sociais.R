e5afe60c4969e147a19ada91d1ab0c7561ecc756
Jefferson Silva <fj_adm@hotmail.com>
2025-07-07 20:52:55 -0300

    CCD - 04 - 05 - Script_ Análise de Redes Sociais

---
+++ CCD - 04 - 05 - Script_ Análise de Redes Sociais.R
@@ -0,0 +1,39 @@
+
+#Carregando a Biblioteca igraph
+library(igraph)
+
+#########################################################################
+#EXEMPLO 1: Importar os arquivos de nós e links
+getwd()
+setwd("D:/WORKSPACES/workspace-vincit/01-INTELIGENCIA_ARTIFICIAL/D003-DATA_CIENCE_R/workshop-posgrad-dataciense-r-rstudio/Arquivos")
+getwd()
+nodes = read.csv("Dataset1-Media-Example-NODES.csv", header=T, as.is=T)
+links = read.csv("Dataset1-Media-Example-EDGES.csv", header=T, as.is=T)
+
+#Examinar os dados importados
+head(nodes)
+head(links)
+nrow(nodes); length(unique(nodes$id))
+nrow(links); nrow(unique(links[,c("from", "to")]))
+
+#"Limpando" os dados
+links = aggregate(links[,3], links[,-3], sum)
+links = links[order(links$from, links$to),]
+colnames(links)[4] = "weight"
+
+#########################################################################
+#EXEMPLO 2: Importar os arquivos de nós e links
+nodes2 = read.csv("Dataset2-Media-User-Example-NODES.csv", header=T, as.is=T)
+links2 = read.csv("Dataset2-Media-User-Example-EDGES.csv", header=T, row.names=1)
+head(nodes2)
+head(links2) #matriz de adjac�ncia
+
+#########################################################################
+#Criando objetos do tipo rede
+net = graph_from_data_frame(d=links, vertices=nodes, directed=T)
+net
+head(net)
+
+net2 = graph_from_incidence_matrix(links2)
+net2
+head(net2)
